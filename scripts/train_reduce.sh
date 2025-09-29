7c1a18a4bbbce83df82ea2b0348e3c700d3e86ba
leechungpa <leechungpa@gmail.com>
2025-09-30 07:52:44 +0900

    merge train_reduce with with_eval

--- scripts/train_reduce.sh
+++ scripts/train_reduce.sh
@@ -26,6 +26,8 @@ declare -A targets=(
   ["all"]="all"
 )
 
+max_samples=100
+
 for rank in "${ranks_list[@]}"; do
 for target in "${!targets[@]}"; do
 
@@ -42,6 +44,16 @@ llamafactory-cli train scripts/train.yaml \
   output_dir="${OUTDIR}${suffix}_epoch1" \
   dataset="mmlu_train_cot_s5"
 
+for shot in 0 3 5; do
+llamafactory-cli eval scripts/eval.yaml \
+    model_name_or_path="$model_name_or_path" \
+    adapter_name_or_path="${OUTDIR}${suffix}_epoch1" \
+    save_dir="${OUTDIR}${suffix}_epoch1/eval_s${shot}" \
+    n_shot=$shot \
+    max_samples=$max_samples
+done
+
+
 for i in 2 3 4 5 6; do
   prev=$((i-1))
   next=$((i))
@@ -55,6 +67,15 @@ for i in 2 3 4 5 6; do
     lora_rank="$rank" \
     output_dir="${OUTDIR}${suffix}_epoch${next}" \
     dataset="mmlu_train_cot_s${nshot}"
+
+  for shot in 0 3 5; do
+  llamafactory-cli eval scripts/eval.yaml \
+      model_name_or_path="$model_name_or_path" \
+      adapter_name_or_path="${OUTDIR}${suffix}_epoch${next}" \
+      save_dir="${OUTDIR}${suffix}_epoch${next}/eval_s${shot}" \
+      n_shot=$shot \
+      max_samples=$max_samples
+  done
 done
 
 # Reduce the weight of 5 shots over 6 epochs
@@ -69,6 +90,15 @@ llamafactory-cli train scripts/train.yaml \
   dataset="mmlu_train_cot_s5,mmlu_train_cot_s0" \
   interleave_probs="0.5,0.5" \
   mix_strategy="interleave_under"
+
+for shot in 0 3 5; do
+llamafactory-cli eval scripts/eval.yaml \
+    model_name_or_path="$model_name_or_path" \
+    adapter_name_or_path="${OUTDIR}${suffix}_epoch1" \
+    save_dir="${OUTDIR}${suffix}_epoch1/eval_s${shot}" \
+    n_shot=$shot \
+    max_samples=$max_samples
+done
 
 for i in 2 3 4 5 6; do
   prev=$((i-1))
@@ -86,6 +116,15 @@ for i in 2 3 4 5 6; do
     dataset="mmlu_train_cot_s5,mmlu_train_cot_s0" \
     interleave_probs="${prob_s5},${prob_s0}" \
     mix_strategy="interleave_under"
+
+  for shot in 0 3 5; do
+  llamafactory-cli eval scripts/eval.yaml \
+      model_name_or_path="$model_name_or_path" \
+      adapter_name_or_path="${OUTDIR}${suffix}_epoch${next}" \
+      save_dir="${OUTDIR}${suffix}_epoch${next}/eval_s${shot}" \
+      n_shot=$shot \
+      max_samples=$max_samples
+  done
 done
 
 done
