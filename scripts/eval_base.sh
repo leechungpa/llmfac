c2a8c5a5115db36db74637de9936122b7ca52911
leechungpa <leechungpa@gmail.com>
2025-09-30 20:55:34 +0000

    modify script to n=100

--- scripts/eval_base.sh
+++ scripts/eval_base.sh
@@ -3,30 +3,19 @@ export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
 export CUDA_VISIBLE_DEVICES=0
-# min_count=9999
-# CUDA_VISIBLE_DEVICES=-1
-# for id in 0 1; do
-#   count=$(nvidia-smi -i $id | grep -c " C ")
-#   echo "- GPU $id has $count processes"
-#   if [ "$count" -lt "$min_count" ]; then
-#     min_count=$count
-#     export CUDA_VISIBLE_DEVICES=$id
-#   fi
-# done
-# echo "Selected GPU: $CUDA_VISIBLE_DEVICES"
 
 set -euo pipefail
 
-OUTDIR="./results"
-model_name_or_path="Qwen/Qwen2.5-3B-Instruct" # "Qwen/Qwen2.5-7B-Instruct"
+OUTDIR="results"
+model_name_or_path="Qwen/Qwen2.5-3B-Instruct" # "Qwen/Qwen2.5-3B-Instruct"  "Qwen/Qwen2.5-7B-Instruct"
 
-max_samples=100
+testset_size=100
+shots=(0 5 10)
 
-
-for shot in 0 3 5; do
+for shot in "${shots[@]}"; do
 llamafactory-cli eval scripts/eval.yaml \
+    task="mmlucot_n${testset_size}" \
     model_name_or_path=$model_name_or_path \
-    save_dir="${OUTDIR}/${model_name_or_path}/base/n${max_samples}_s${shot}" \
-    n_shot=$shot \
-    max_samples=$max_samples
+    save_dir="${OUTDIR}/eval/${model_name_or_path}/base/n${testset_size}_s${shot}" \
+    n_shot=$shot
 done
--- scripts/eval_checkpoint.sh
+++ scripts/eval_checkpoint.sh
@@ -2,25 +2,18 @@ export HF_HOME=/data/cl/hf
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 set -euo pipefail
 
-max_samples=100
-model_name_or_path="Qwen/Qwen2.5-7B-Instruct" # "Qwen/Qwen2.5-7B-Instruct"
+OUTDIR="results"
+model_name_or_path="Qwen/Qwen2.5-3B-Instruct" # "Qwen/Qwen2.5-3B-Instruct"  "Qwen/Qwen2.5-7B-Instruct"
+suffix="all_r128_epoch6"
 
-OUTDIR="./results"
-CKPT_ROOT="/home/cl/llmfac/results/Qwen/Qwen2.5-7B-Instruct/all_r4_epoch6"
+testset_size=100
+shots=(0 5 10)
 
-shots=(0 5)
-
-# Collect checkpoint directories
-mapfile -d '' ckpt_dirs < <(find "$CKPT_ROOT" -maxdepth 1 -type d -name 'checkpoint*' -print0 | sort -z)
-
-if [[ ${#ckpt_dirs[@]} -eq 0 ]]; then
-  echo "No checkpoint directories found under $CKPT_ROOT"
-  exit 1
-fi
+mapfile -d '' ckpt_dirs < <(find "${OUTDIR}/${model_name_or_path}/${suffix}" -maxdepth 1 -type d -name 'checkpoint*' -print0 | sort -z)
 
 for ckpt in "${ckpt_dirs[@]}"; do
   ckpt="${ckpt%/}"
@@ -29,11 +22,10 @@ for ckpt in "${ckpt_dirs[@]}"; do
 
   for shot in "${shots[@]}"; do
     llamafactory-cli eval scripts/eval.yaml \
+      task="mmlucot_n${testset_size}" \
       model_name_or_path="$model_name_or_path" \
       adapter_name_or_path="${ckpt}" \
-      save_dir="${OUTDIR}/eval/${model_name_or_path}/${name}/n${max_samples}_s${shot}" \
-      n_shot="$shot" \
-      max_samples="$max_samples"
+      save_dir="${OUTDIR}_eval/${model_name_or_path}/${suffix}/${name}-n${testset_size}_s${shot}" \
+      n_shot="$shot"
   done
 done
-
