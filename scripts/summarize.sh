c7a33c3bd893dca1d4443a5543236a9a5c5f5cbf
leechungpa <leechungpa@gmail.com>
2025-10-29 13:28:41 +0900

    add script for summarizing

---
+++ scripts/summarize.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+model_name="Qwen/Qwen2.5-3B-Instruct"
+category="Humanities"
+
+eval_dir="/home/$(whoami)/llmfac/results_eval/${model_name}"
+
+
+##################################################
+eval_dir_base="${eval_dir}/base"
+eval_dir_finetuned="${eval_dir}/${category}/qkv_r128_epoch5_lr1.0e-5"
+
+python src/utils/summarize.py  \
+  --base_dir "${eval_dir_base}/log" "${eval_finetuned}/log" \
+  --output_dir "${eval_finetuned}"
+
+##################################################
+# Summarize MMLU evaluation results for each finetuned run.
+eval_dir_base="${eval_dir}/base"
+eval_dir_finetuned="${eval_dir}/${category}"
+
+for eval_finetuned in "${eval_dir_finetuned}"/*; do
+  if [ -d "$eval_finetuned" ]; then
+    echo "Processing $eval_finetuned..."
+    python src/utils/summarize.py \
+      --base_dir "${eval_dir_base}/log" "${eval_finetuned}/log" \
+      --output_dir "${eval_finetuned}"
+  fi
+done
+
+##################################################
+# Summarize word counting results for each finetuned run.
+eval_dir_base="${eval_dir}/counts/base"
+eval_dir_finetuned="${eval_dir}/counts/${category}"
+
+for eval_finetuned in "${eval_dir_finetuned}"/*; do
+  if [ -d "$eval_finetuned" ]; then
+    echo "Processing $eval_finetuned..."
+    python src/utils/summarize.py \
+      --base_dir "${eval_dir_base}/log" "${eval_finetuned}/log" \
+      --output_dir "${eval_finetuned}"
+  fi
+done
